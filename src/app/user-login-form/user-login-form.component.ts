56b305523a9627ac3169fc7916b428f4e558a097
PinkyThomasIG <pinkymukalal@gmail.com>
2025-04-28 10:23:21 +0200

    user profile component has been added

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { UserRegistrationService } from '../fetch-api-data.service';
+import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login-form',
+  imports: [
+    MatCardModule,
+    MatFormField,
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss'],
 })
@@ -15,27 +28,25 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) {}
 
   ngOnInit(): void {}
 
-  loginUser(): void {
+  userLogin(): void {
     this.fetchApiData.userLogin(this.userData).subscribe(
       (result) => {
-        // Save username and token to localStorage
+        localStorage.setItem('user', JSON.stringify(result.user));
         localStorage.setItem('token', result.token);
-        localStorage.setItem('user', result.user.Username);
-
-        this.dialogRef.close(); // Close modal
-        this.snackBar.open('Login successful!', 'OK', {
+        this.dialogRef.close();
+        this.snackBar.open('Login successful', 'OK', {
           duration: 2000,
         });
-
         this.router.navigate(['movies']);
       },
-      (error) => {
-        this.snackBar.open('Login failed. Check your credentials.', 'OK', {
+      (result) => {
+        this.snackBar.open('Login failed' + result, 'OK', {
           duration: 2000,
         });
       }
